f68d32ad1c91223370a9899936044a6991ac270d
KEntaro <kenkou.ishi.1111@gmail.com>
2025-03-31 08:35:11 -0400

    modified

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -30,30 +30,31 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # Django管理サイトへのパス
 
     # djangoappアプリケーションのURL設定をインクルード
-    path('djangoapp/', include('djangoapp.urls')),  # djangoappアプリケーションのURL設定をインクルード
+    path('djangoapp/', include('djangoapp.urls')),
 
     # ホームページのテンプレートビュー設定
-    path('', TemplateView.as_view(template_name="Home.html")),  # ホームページ用のテンプレートビュー
+    path('', TemplateView.as_view(template_name="Home.html")),
 
     # アバウトページのテンプレートビュー設定
-    path('about/', TemplateView.as_view(template_name="About.html")),  # アバウトページ用のテンプレートビュー
+    path('about/', TemplateView.as_view(template_name="About.html")),
 
     # お問い合わせページのテンプレートビュー設定
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),  # お問い合わせページ用のテンプレートビュー
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
 
     # ログインページのテンプレートビュー設定
-    path('login/', TemplateView.as_view(template_name="index.html")),  # ログインページ用のテンプレートビュー
+    path('login/', TemplateView.as_view(template_name="index.html")),
 
     # 登録ページのテンプレートビュー設定
-    path('register/', TemplateView.as_view(template_name="index.html")),  # 登録ページ用のテンプレートビュー
+    path('register/', TemplateView.as_view(template_name="index.html")),
 
     # dealersページのテンプレートビュー設定
-    path('dealers/', TemplateView.as_view(template_name="index.html")),  # dealersページ用のテンプレートビュー
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
 
     # dealerページのテンプレートビュー設定
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),  # dealerページ用のテンプレートビュー
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 
     # postreviewページのテンプレートビュー設定
     path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # 静的ファイルのルーティング設定
+    # 静的ファイルのルーティング設定
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
