d27746975b0e7459ca6e31c75c7f3815d966b22f
Kentaro <kenkou.ishi.1111@gmail.com>
2025-03-31 06:17:39 -0400

    modified

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Djangoのモデルコードを追加する前に、以下のインポートを解除してください
 
 from django.db import models  # Djangoのデータベースモデルをインポート
-from django.utils.timezone import now  # 現在の日付と時刻を取得するためにインポート
 from django.core.validators import MaxValueValidator, MinValueValidator  # バリデーターをインポート
 
 # 車のメーカーを表すモデル
@@ -19,7 +18,9 @@ class CarMake(models.Model):
 # 車のモデルを表すモデル
 class CarModel(models.Model):
     # CarMakeモデルとの多対一（Many-to-One）関係を定義
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # CarMakeが削除されると、関連するCarModelも削除される
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE
+    )  # CarMakeが削除されると、関連するCarModelも削除される
     # 車のモデル名を格納するフィールド（最大100文字）
     name = models.CharField(max_length=100)
     # 車のタイプを選択肢から選ぶためのフィールド
@@ -30,18 +31,23 @@ class CarModel(models.Model):
         # 必要に応じて他の選択肢を追加
     ]
     # 車のタイプを選択肢から選ぶためのフィールド（デフォルトはSUV）
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    type = models.CharField(
+        max_length=10, choices=CAR_TYPES, default='SUV'
+    )
     # 車の年式を格納するフィールド（2015年から2023年の範囲に制限）
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),  # 最大値は2023年
-            MinValueValidator(2015)   # 最小値は2015年
-        ])
+            MinValueValidator(2015),   # 最小値は2015年
+        ]
+    )
     # 必要に応じて他のフィールドを追加
 
     # オブジェクトを文字列として返すためのメソッド
     def __str__(self):
         return self.name  # 名前を文字列として返す
+
 
 # <ヒント> 車のメーカー（CarMake）モデルを作成 `class CarMake(models.Model)`:
 # - name: 車のメーカー名
